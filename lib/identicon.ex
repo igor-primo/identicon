828480ae0246455adb254de29342df2f64557642
igor-primo <igorprimo62@gmail.com>
2024-09-01 01:34:37 -0300

    feat: add capability to build the grid. Also add fucking parenthesis

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,20 +9,29 @@ defmodule Identicon do
   @doc """
   """
 
-  def main input do
+  def main(input) do
     input
     |> hash_input
     |> pick_color
     |> build_grid
   end
 
-  def build_grid %Image{hex: hex} do
-    hex
-    |> Enum.chunk_every(3, 3, :discard)
-    |> Enum.map(&mirror_row/1)
+  @doc """
+    Builds the grid that stores the number of each Identicon block
+    and its index.
+  """
+  def build_grid(%Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
   end
 
-  def mirror_row [fst, snd | _tail] = row do
+  def mirror_row([fst, snd | _tail] = row) do
     row ++ [snd, fst]
   end
 
@@ -30,7 +39,7 @@ defmodule Identicon do
     Picks the first 3 numbers from the list of representation
     of the md5 binary hash. These are the RGB.
   """
-  def pick_color %Image{hex: [r, g, b | _tail ]} = image do
+  def pick_color(%Image{hex: [r, g, b | _tail]} = image) do
     # Is not pattern matching and equational reasoning lovely, delicious?
     %Image{image | color: {r, g, b}}
   end
@@ -40,7 +49,7 @@ defmodule Identicon do
     the result as a binary representaiton, only as
     a list.
   """
-  def hash_input input do
-    %Identicon.Image{hex: :crypto.hash(:md5, input) |> :binary.bin_to_list}
+  def hash_input(input) do
+    %Identicon.Image{hex: :crypto.hash(:md5, input) |> :binary.bin_to_list()}
   end
 end
