7f3d70452899f2881fab8edf76edb136cb01b726
igor-primo <igorprimo62@gmail.com>
2024-08-31 21:21:35 -0300

    feat: add main function and first transformation, hashing

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,15 +4,15 @@ defmodule Identicon do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
   """
-  def hello do
-    :world
+
+  def main input do
+    input
+    |> hash_input
+  end
+
+  def hash_input input do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
